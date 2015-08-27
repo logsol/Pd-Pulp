38907d6dd0eb7a74ba77c3560676080d05e62371
logsol <logspum@gmail.com>
2015-08-27 20:02:37 +0200

    remove intermediate build stuff from repo

--- Builds/MacOSX/build/Intermediates/Pd Pulp FX.build/Debug/Pd Pulp FX.build/Objects-normal/i386/PdBase.d
+++
@@ -1,8 +0,0 @@
-dependencies: /Users/logsol/Work/projects/c++/JuceLibPd/Source/PdBase.cpp \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/PdBase.hpp \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/PdReceiver.hpp \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/PdTypes.hpp \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/PdMidiReceiver.hpp \
-  ../../libpd/libpd_wrapper/z_libpd.h ../../libpd/pure-data/src/m_pd.h \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/z_queued.h \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/z_print_util.h
--- Builds/MacOSX/build/Intermediates/Pd Pulp FX.build/Debug/Pd Pulp FX.build/Objects-normal/i386/PdTypes.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/PdTypes.cpp \
-  /Users/logsol/Work/projects/c++/JuceLibPd/Source/PdTypes.hpp
