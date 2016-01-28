06e8fb91ea3be59f17d403b13adfa3278d92e61b
logsol <logspum@gmail.com>
2016-01-29 00:46:19 +0100

    some clean up

--- Builds/MacOSX/build/Intermediates/Pd Pulp FX.build/Debug/Pd Pulp FX.build/Objects-normal/i386/juce_AAX_Wrapper-20CB2782983A2CA4.d
+++ Builds/MacOSX/build/Intermediates/Pd Pulp FX.build/Debug/Pd Pulp FX.build/Objects-normal/i386/juce_AAX_Wrapper-20CB2782983A2CA4.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /Users/logsol/Work/projects/c++/JuceLibPd/JuceLibraryCode/modules/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp \
-  /Users/logsol/Work/projects/c++/JuceLibPd/JuceLibraryCode/AppConfig.h \
-  /Users/logsol/Work/projects/c++/JuceLibPd/JuceLibraryCode/modules/juce_audio_plugin_client/AAX/../utility/juce_CheckSettingMacros.h
+  /Users/logsol/Work/projects/c++/PdPulp/JuceLibraryCode/modules/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp \
+  /Users/logsol/Work/projects/c++/PdPulp/JuceLibraryCode/AppConfig.h \
+  /Users/logsol/Work/projects/c++/PdPulp/JuceLibraryCode/modules/juce_audio_plugin_client/AAX/../utility/juce_CheckSettingMacros.h
--- Builds/MacOSX/build/Intermediates/Pd Pulp FX.build/Debug/Pd Pulp FX.build/Objects-normal/i386/juce_VST3_Wrapper-17941E2E5C299BD.d
+++ Builds/MacOSX/build/Intermediates/Pd Pulp FX.build/Debug/Pd Pulp FX.build/Objects-normal/i386/juce_VST3_Wrapper-17941E2E5C299BD.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /Users/logsol/Work/projects/c++/JuceLibPd/JuceLibraryCode/modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.mm \
-  /Users/logsol/Work/projects/c++/JuceLibPd/JuceLibraryCode/AppConfig.h \
-  /Users/logsol/Work/projects/c++/JuceLibPd/JuceLibraryCode/modules/juce_audio_plugin_client/VST3/../utility/juce_CheckSettingMacros.h
+  /Users/logsol/Work/projects/c++/PdPulp/JuceLibraryCode/modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.mm \
+  /Users/logsol/Work/projects/c++/PdPulp/JuceLibraryCode/AppConfig.h \
+  /Users/logsol/Work/projects/c++/PdPulp/JuceLibraryCode/modules/juce_audio_plugin_client/VST3/../utility/juce_CheckSettingMacros.h
